f29fe987a7dc9c12a3413becf7c6e6389c6e75e8
sergio garcia <sergio.garcia.sanjuan@gmail.com>
2022-11-23 13:36:38 +0100

    Sections tree working for two levels

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,8 +1,14 @@
 class SectionsController < ApplicationController
   before_action :set_section, only: %i[show edit update]
+  before_action :set_breadcrumbs, only: %i[index show new edit]
 
   def index
-    authorize @sections = policy_scope(Section.all).order(:order)
+    # authorize @sections = policy_scope(Section).includes(:sections, :questions).all.order(:order, :created_at, :section_id)
+    # authorize @sections = policy_scope(Section).joins(:sections).includes(:sections, :questions).all.order(:order)
+    # authorize @sections = policy_scope(Section).left_joins(:sections).distinct.all.order(section_id: :desc).order(:order)
+    authorize @sections = policy_scope(Section).includes(:sections, :questions).all.order(:order)
+
+    # .left_joins(:sections).where(sections: { section_id: !nil })
   end
 
   def new
@@ -24,13 +30,11 @@ class SectionsController < ApplicationController
   end
 
   def update
-    # parent = Section.find(params["section"]["section_id"].to_i) unless params["section"]["section_id"].empty?
     parent = params["section"]["section_id"]
-    # parent = nil if parent.empty?
     parent.empty? ? parent = nil : parent = Section.find(params["section"]["section_id"].to_i)
     @section.section = parent
     if @section.update(section_params)
-      redirect_to section_path(@section)
+      redirect_to sections_path
     else
       render :edit, status: :unprocessable_entity
     end
@@ -44,5 +48,13 @@ class SectionsController < ApplicationController
 
   def set_section
     authorize @section = Section.find(params[:id])
+  end
+
+  def set_breadcrumbs
+    add_breadcrumb "Home", '/'
+    add_breadcrumb "Sections", sections_path
+    add_breadcrumb "New", new_section_path if action_name == 'new'
+    # add_breadcrumb "Patient details", "/patients/#{@patient.id}" if action_name == 'show' || action_name == 'edit'
+    add_breadcrumb "Edit details", "/sections/#{@section.id}/edit" if action_name == 'edit'
   end
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < ApplicationRecord
   validates :title, presence: true, allow_blank: false
   validates :title_to_display, presence: true, allow_blank: false
   validates :order, presence: true, allow_blank: false, numericality: { only_integer: true }
-  has_many :questions
-  has_many :sections, dependent: :destroy
+  has_many :questions, -> { order(order: :asc) }
+  has_many :sections, -> { order(order: :asc) }, dependent: :destroy
   belongs_to :section, optional: true
 end
